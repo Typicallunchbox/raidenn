d744b466607f7989a11f19eee62ff202c282e2d8
KEEGAN LAUNSPACH <keegan.launspach1@hotmail.com>
2022-06-30 23:07:54 +0200

    commit comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
+import '../index.scss'
+import axios from 'axios'
 
 export const Home = () => {
+
+  const[movies,setMovies]=useState([]);
+
+  //get popular movies
+  //get popular series
+  // get genres
+  // get upcoming
+  //get similiar movies
+  // api rate endpoint available
+  //discover movies endpoint available
+
+  //example : https://api.themoviedb.org/3/movie/popular?api_key=120fe4d587d5f86c44f0a6e599f01734
+    
+  useEffect(()=>{
+    axios.get('https://api.themoviedb.org/3?api_key=120fe4d587d5f86c44f0a6e599f01734').then(resp => {
+      // setMovies(resp.data);
+      console.log(resp.data)
+  });
+  },[])
   return (
-    <div>home</div>
+    <div className='home container'>
+      <div className='showcase'>hi</div>
+      {movies && <p>{movies}</p>}
+      <div className='catalogue'></div>
+    </div>
   )
 }
