da30e28e5afba4d80779231cb00b56b434222c4a
KEEGAN LAUNSPACH <keegan.launspach1@hotmail.com>
2022-07-06 23:10:13 +0200

    Tidied code and added on hover effects

--- src/components/home-top-option/HomeTopOption.jsx
+++ src/components/home-top-option/HomeTopOption.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from "react";
+import "../home-top-option/HomeTopOption.scss"
 import axios from "axios";
 
 
--- src/components/item-catalogue-list/ItemCatalogueList.jsx
+++ src/components/item-catalogue-list/ItemCatalogueList.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from "react";
+import "../item-catalogue-list/ItemCatalogueList.scss"
 import axios from "axios";
 
 const ItemCatalogueList = () => {
